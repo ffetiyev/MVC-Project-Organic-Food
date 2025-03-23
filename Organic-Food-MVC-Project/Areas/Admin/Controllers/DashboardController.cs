d44fb4c9704b1d17c58580a23b8a59becb91d773
[ffetiyev] <[faridhf@code.edu.az]>
2025-03-24 01:54:40 +0400

    Project succesfully done

--- Organic-Food-MVC-Project/Areas/Admin/Controllers/DashboardController.cs
+++ Organic-Food-MVC-Project/Areas/Admin/Controllers/DashboardController.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace Organic_Food_MVC_Project.Areas.Admin.Controllers
 {
     [Area("Admin")]
+    [Authorize(Roles ="SuperAdmin,Admin")]
     public class DashboardController : Controller
     {
         public IActionResult Index()
--- Organic-Food-MVC-Project/Program.cs
+++ Organic-Food-MVC-Project/Program.cs
@@ -70,8 +70,8 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
-app.UseAuthorization();
 app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "areas",
